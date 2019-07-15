cdf9f56ce8c53a024f77daaa07cad49cfa5520ce
yangjinping <yangjinping@inmcn.com>
2019-07-15 16:15:46 +0800

    xpzi-tijiao

---
+++ src/pages/Index/home/component/HomeImg.vue
@@ -0,0 +1,26 @@
+<template>
+    <a href="data[0].action" class="home-img" >
+        <img src="data[0].headImg">
+    </a>
+</template>
+<script>
+
+export default {
+    props: ['data'],
+    computed: {
+        homeNavData(){
+            return this.data;
+        }
+    }
+    
+}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/ellipsis.styl'
+.home-img
+    height 44px
+
+    img
+        height 44px
+        
+</style>
